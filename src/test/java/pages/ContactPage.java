75939d9a5111358c712827914774ba3b4f8dc23b
Dmitrii <Apocaliptic.love@gmail.com>
2023-03-15 13:25:55 +0400

    PageObject,Config

--- src/test/java/pages/ContactPage.java
+++ src/test/java/pages/ContactPage.java
@@ -1,32 +1,39 @@
 package pages;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.SelenideElement;
+
+import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ContactPage {
 
-    public static final String PROFILE_NAME_1 = "Andrey Veselov";
-    public static final String PROFILE_NAME_2 = "Eugene Bezrodnyy";
-    public static final String PROFILE_NAME_3 = "Alexander Akkaev";
-
-    private SelenideElement
-    profile1 = $(byTagAndText("h3", "Andrey Veselov")),
-    profile2 = $(byTagAndText("h3", "Eugene Bezrodnyy")),
-    profile3 = $(byTagAndText("h3", "Alexander Akkaev"));
+//    public static final String PROFILE_NAME_1 = "Andrey Veselov";
+//    public static final String PROFILE_NAME_2 = "Eugene Bezrodnyy";
+//    public static final String PROFILE_NAME_3 = "Alexander Akkaev";
+//
+//    private SelenideElement
+//    profile1 = $(byTagAndText("h3", "Andrey Veselov")),
+//    profile2 = $(byTagAndText("h3", "Eugene Bezrodnyy")),
+//    profile3 = $(byTagAndText("h3", "Alexander Akkaev"));
 
     public ContactPage openContact(){
         open("https://rollerads.com/contact.html");
         return this;
     }
-    public ContactPage checkTeamList(){
-        profile1.shouldHave(text(PROFILE_NAME_1));
-        profile2.shouldHave(text(PROFILE_NAME_2));
-        profile3.shouldHave(text(PROFILE_NAME_3));
+//    public ContactPage checkTeamList(){
+//        profile1.shouldHave(text(PROFILE_NAME_1));
+//        profile2.shouldHave(text(PROFILE_NAME_2));
+//        profile3.shouldHave(text(PROFILE_NAME_3));
+//        return this;
+//    }
+
+    public ContactPage checkTeamList(List<String> expectedName){
+        $$("h3").shouldHave(CollectionCondition.texts(expectedName));
         return this;
     }
 }
--- src/test/java/tests/ContactPageContentTests.java
+++ src/test/java/tests/ContactPageContentTests.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.ContactPage;
 
+import java.util.List;
+
 import static io.qameta.allure.Allure.step;
 
 public class ContactPageContentTests extends TestBase {
 
     ContactPage contactPage = new ContactPage();
+
+    List<String> expectedName = List.of(
+            "Andrey Veselov",
+            "Eugene Bezrodnyy",
+            "Alexander Akkaev");
 
     @DisplayName("Отображение всех имен и фамилий в разделе Contact")
     @Test
@@ -17,7 +24,7 @@ public class ContactPageContentTests extends TestBase {
             contactPage.openContact();
         });
         step("Првоерить, что все имена отображаются", () -> {
-            contactPage.checkTeamList();
+            contactPage.checkTeamList(expectedName);
         });
     }
 }
